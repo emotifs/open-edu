558f4855e106a653f2c39fc1fb58a9c5aec1da8a
dilbekwebdev <dilbek.webdev@mail.ru>
2021-08-28 23:28:57 +0500

    changed some texts

--- src/user/SignUp.js
+++ src/user/SignUp.js
@@ -14,27 +14,27 @@ function SignUp() {
                <form>
                 <h3>Ro'yxatdan o'tish</h3>
 
-                <div className="form-group my-3">
+                <div className=" my-3">
                     <label>Ism</label>
                     <input type="text" className="form-control" placeholder="First name" />
                 </div>
 
-                <div className="form-group my-3">
+                <div className=" my-3">
                     <label>Familya</label>
                     <input type="text" className="form-control" placeholder="Last name" />
                 </div>
               
-                <div className="form-group my-3">
+                <div className=" my-3">
                     <label>Email address</label>
                     <input type="email" className="form-control" placeholder="Enter email" />
                 </div>
 
-                <div className="form-group my-3">
+                <div className="my-3">
                     <label>Parol</label>
                     <input type="password" className="form-control" placeholder="Enter password" />
                 </div>
 
-                <div className="form-group my-3">
+                <div className=" my-3">
                     <label>Nimaga qiziqasiz?</label>
                     <select name="roads" id="roads" className="form-control">
                          <option value="schoolarships">Schoolarships</option>
