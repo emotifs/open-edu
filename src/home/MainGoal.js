a2fdbcb6bb86860bc63189270717edd10b17a003
dilbekwebdev <dilbek.webdev@mail.ru>
2021-08-29 14:49:01 +0500

    added home animations

--- src/home/MainGoal.js
+++ src/home/MainGoal.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Container} from 'reactstrap';
 import {Link} from 'react-router-dom';
+import Flash from 'react-reveal/Flash';
 
 function MainGoal() {
     return (
@@ -10,7 +11,10 @@ function MainGoal() {
                 <p>
                 Ilm qayerda bo'lishidan qattiy nazar uni egallash va tarqatish bizning  burchimizdir, <br />majburiyatimizdir. Shunday ekan biz bilan eng zo'rlariga erishing
                 </p>
-                <Link to="/sign-up" className="btn btn-light py-3 px-4">Ro'yxatdan o'tish</Link>
+                <Flash>
+                    <Link to="/sign-up" className="btn btn-light py-3 px-4">Ro'yxatdan o'tish</Link>
+                </Flash>
+               
             </Container>
         </div>
     )
